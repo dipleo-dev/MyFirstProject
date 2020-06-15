a4dd7c1442f86a7adaa594b435ea2cb14ce4ac40
dipleo-dev <dipleo.dev@gmail.com>
2020-06-15 14:13:59 +0200

    first commit

---
+++ first/src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const LandingPage=()=>
+{
+    return(
+    <div>
+    <h1 style={{color:"red"}}>The Avengers</h1>
+    <img src="https://4001reviews.de/wp-content/uploads/2018/02/Titelbild-Zusammenfassung-Avengers-Filmreihe.jpg" alt=""/>
+    <p>Avengers age of Ultron</p>
+    <button type="button">Change Picture</button>
+    </div>
+
+)
+}
+export default LandingPage
